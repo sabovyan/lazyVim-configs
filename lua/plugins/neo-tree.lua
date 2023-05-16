8f3229d55bde55069ed99ec52b51971e5dcea797
sabovyan <abovyan.sargis90@gmail.com>
2023-05-16 10:22:03 +0400

    handle markdown issue

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,35 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    filesystem = {
+      follow_current_file = true,
+      use_libuv_file_watcher = true,
+      filtered_items = {
+        visible = true,
+        hide_dotfiles = false,
+        hide_gitignored = true,
+        never_show = {
+          ".git",
+        },
+      },
+    },
+
+    default_component_configs = {
+      git_status = {
+        symbols = {
+          -- Change type
+          added = "", -- or "✚", but this is redundant info if you use git_status_colors on the name
+          modified = "M", -- or "", but this is redundant info if you use git_status_colors on the name
+          deleted = "✖", -- this can only be used in the git_status source
+          renamed = "", -- this can only be used in the git_status source
+          -- Status type
+          untracked = "U",
+          ignored = "",
+          unstaged = "", -- "",
+          staged = "",
+          conflict = "",
+        },
+      },
+    },
+  },
+}
