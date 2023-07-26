cb3fc25c88a8c5dd0d46edb3ba281d65655c4061
sabovyan <abovyan.sargis90@gmail.com>
2023-07-26 09:50:05 +0400

    remove telescope version

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,7 +1,6 @@
 -- Fuzzy Finder (files, lsp, etc)
 return {
   "nvim-telescope/telescope.nvim",
-  version = "*",
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-telescope/telescope-live-grep-args.nvim",
