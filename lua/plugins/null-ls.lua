8f35127b6a83662376b4707347bc06b8bb79f726
sabovyan <abovyan.sargis90@gmail.com>
2023-08-02 13:43:21 +0400

    remove unused lines

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -7,12 +7,9 @@ return {
     return {
       root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git"),
       sources = {
-        -- nls.builtins.formatting.fish_indent,
-        -- nls.builtins.diagnostics.fish,
         nls.builtins.formatting.stylua,
         nls.builtins.formatting.shfmt,
         nls.builtins.formatting.cbfmt,
-        -- nls.builtins.diagnostics.flake8,
       },
     }
   end,
