3410acb58e8233597aea7bee443b4ae840cf81cb
sabovyan <abovyan.sargis90@gmail.com>
2023-10-16 21:31:10 +0400

    add more language servers

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -6,6 +6,12 @@ return {
       "shellcheck",
       "shfmt",
       "svelte-language-server",
+      "typescript-language-server",
+      "html-lsp",
+      "css-lsp",
+      "tailwindcss-language-server",
+      "graphql-language-service-cli",
+      "emmet-language-server",
     },
   },
 }
